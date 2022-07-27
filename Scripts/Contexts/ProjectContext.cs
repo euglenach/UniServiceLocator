256f15f03198459db5b6d061eafd3306064e0b3d
euglenach <midorimushiiii@gmail.com>
2022-07-27 18:48:07 +0900

    ProjectContext

--- Scripts/Contexts/ProjectContext.cs
+++ Scripts/Contexts/ProjectContext.cs
@@ -1,7 +1,37 @@
-﻿namespace UniServiceLocator
+﻿using System;
+using UnityEngine;
+
+namespace UniServiceLocator
 {
-    public class ProjectContext
+    [DefaultExecutionOrder(-9999)]
+    public class ProjectContext : MonoBehaviour
     {
+        public Locator Locator{get;private set;} = new Locator();
         
+        private static ProjectContext inst;
+        public ProjectContext Inst
+        {
+            get
+            {
+                if(inst == null) inst = FindObjectOfType<ProjectContext>();
+                if(inst == null) inst = new GameObject("ProjectContext").AddComponent<ProjectContext>();
+                return inst;
+            }
+        }
+        
+        private void Awake()
+        {
+            if(!Application.isPlaying) return;
+            
+            if(inst == null)
+            {
+                inst = this;
+                DontDestroyOnLoad(gameObject);
+                return;
+            }
+            
+            Destroy(gameObject);
+        }
+
     }
 }
