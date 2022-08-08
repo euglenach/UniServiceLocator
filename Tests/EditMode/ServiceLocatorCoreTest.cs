3c276d59efbbd3c6932c26b1a9233d5390d4863d
Euglenach <midorimushiiii@gmail.com>
2022-08-09 04:05:02 +0900

    てすと

---
+++ Tests/EditMode/ServiceLocatorCoreTest.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+
+namespace UniServiceLocator.Tests.EditMode
+{
+    public class EditorTest
+    {
+        [Test]
+        public void ResolveTest1()
+        { 
+            var locator = new ServiceLocator();
+            locator.Register<TestClass1>();
+            
+            Assert.That(locator.Resolve<TestClass1>() != null,Is.True);
+        }
+        
+        [Test]
+        public void ResolveTest2()
+        { 
+            var locator = new ServiceLocator();
+            var test = new TestClass1();
+            locator.Register(test);
+            
+            Assert.That(locator.Resolve<TestClass1>(),Is.EqualTo(test));
+        }
+        
+        [Test]
+        public void ResolveTest3()
+        { 
+            var locator = new ServiceLocator();
+            locator.Register<TestClass1,ITestInterface1>();
+            
+            Assert.That(locator.Resolve<ITestInterface1>() != null,Is.True);
+        }
+    }
+    
+    class TestClass1 : ITestInterface1{}
+
+    interface ITestInterface1{}
+}
+
---
+++ Tests/PlayMode/ProjectContextTest.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace UniServiceLocator.Tests.PlayMode
+{
+    public class ProjectContextTest
+    {
+        [UnityTest]
+        public IEnumerator GenerateProjectContextTest()
+        {
+            ProjectContext.Locator.Register<TestClass1>();
+            yield return null;
+            var projectContext = Object.FindObjectOfType<ProjectContext>();
+            Assert.That(projectContext != null,Is.True);
+        }
+        
+        [UnityTest]
+        public IEnumerator ResolveProjectContextTest()
+        {
+            var test = new TestClass1();
+            yield return null;
+            ProjectContext.Locator.Register<ITestInterFace1>(test);
+            Assert.That(ProjectContext.Locator.Resolve<ITestInterFace1>() != null,Is.True);
+        }
+    }
+    
+    class TestClass1 : ITestInterFace1{}
+    interface ITestInterFace1{}
+}
